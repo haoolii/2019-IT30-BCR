cb8aaad0f477ce9fbd8e7f54642905945ec6891e
unnhao <unnhao@gmail.com>
2019-09-27 14:17:58 +0800

    deploy

--- gameServer/middleware/auth.js
+++ gameServer/middleware/auth.js
@@ -6,6 +6,9 @@ const dbTable = require('../lib/dbTable')
 
 var auth = function (socket, next) {
   console.log(socket.handshake.query.token)
+  console.log('===========================KE')
+  console.log(process.env.SECRET_KEY)
+  console.log('==============================')
   jwt.verify(socket.handshake.query.token, process.env.SECRET_KEY, (err, decoded) => {
     if (err) {
       console.log(err)
